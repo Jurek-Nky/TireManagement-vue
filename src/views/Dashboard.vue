339a4802d909946166633faac1df08c8b32fca04
jurek-nky <jurek.nit@gmail.com>
2022-01-04 19:01:41 +0100

    added a rough layout for the dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,20 @@
 <template>
-<h2>dashboard</h2>
+  <q-page class="row justify-center items-center">
+    <div class="column">
+      <div class="row">
+        <q-card rounded bordered class="q-pa-lg q-ma-lg shadow-5 bg-primary">
+          <q-card-section>
+
+          </q-card-section>
+        </q-card>
+      </div>
+      <q-card rounded bordered class="q-pa-lg q-ma-lg shadow-5 bg-primary">
+        <q-card-section>
+
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script>
