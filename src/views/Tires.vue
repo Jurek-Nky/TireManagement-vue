5e4391e1e75d5b06f4a4c730e99b010c8bdc13da
jurek-nky <jurek.nit@gmail.com>
2022-01-19 03:14:31 +0100

    added pressure calculation to tires page

--- src/views/Tires.vue
+++ src/views/Tires.vue
@@ -14,16 +14,14 @@
     <q-tab-panels v-model="tab" animated class="transparent">
       <q-tab-panel name="in_store" class="row justify-center full-height q-gutter-lg">
         <div class="column">
-          <q-table
-              title="Auf Lager"
-              :rows="tireSetRows_inStock"
-              :columns="tireSetColumns_inStock"
-              row-key="id"
-              class="bg-primary"
-              dark
-              no-data-label="Table is empty"
-              :loading="loading_inStock"
-          >
+          <q-table title="Auf Lager"
+                   :rows="tireSetRows_inStock"
+                   :columns="tireSetColumns_inStock"
+                   row-key="id"
+                   class="bg-primary"
+                   dark
+                   no-data-label="Table is empty"
+                   :loading="loading_inStock">
             <template v-slot:loading>
               <q-inner-loading showing color="primary"/>
             </template>
@@ -45,12 +43,9 @@
                   <q-btn outline rounded size="sm" color="white" @click="props.expand = !props.expand"
                          :icon="props.expand ? 'mdi-minus' : 'mdi-plus'"/>
                 </q-td>
-                <q-td
-                    v-for="col in props.cols"
-                    :key="col.name"
-                    :props="props"
-                >
-
+                <q-td v-for="col in props.cols"
+                      :key="col.name"
+                      :props="props">
                   <div v-if="col.name==='delete'" class="q-gutter-sm">
                     <q-btn icon="mdi-delete" @click="tireSetDelete(props.row)" flat color="white"
                            dense></q-btn>
@@ -74,36 +69,67 @@
               </q-tr>
               <q-tr v-show="props.expand" :props="props">
                 <q-td colspan="100%">
-                  <q-table
-                      title="Tires"
-                      :rows="props.row.tires"
-                      :columns="tireColumns"
-                      row-key="tireID"
-                      hide-bottom
-                      dark
-                      class="bg-primary"
-                      bordered
-                  >
+                  <q-table title="Tires"
+                           :rows="props.row.tires"
+                           :columns="tireColumns"
+                           row-key="tireID"
+                           hide-bottom
+                           dark
+                           class="bg-primary"
+                           bordered>
                   </q-table>
                 </q-td>
               </q-tr>
             </template>
           </q-table>
         </div>
+        <div class="column">
+          <q-card class="bg-primary">
+            <q-card-section class="q-gutter-y-sm">
+              <div class="row justify-center text-white text-subtitle1">Kaltdruck</div>
+              <div class="row items-center q-gutter-x-sm q-mb-md">
+                <q-input dense dark filled v-model="tireTempCold" type="number"
+                         label="Felgentemperatur" @keyup="setAllPressure"/>
+              </div>
+              <div class="row q-gutter-x-sm">
+                <q-input dense dark filled v-model="pressureFL" type="number" label="Vorne Links"
+                         @keyup="setSinglePressure(pressureFL,'FL')"/>
+                <q-input dense dark filled v-model=" pressureFR" type="number" label="Vorne Rechts"
+                         @keyup="setSinglePressure(pressureFR,'FR')"/>
 
+              </div>
+              <div class="row q-gutter-x-sm">
+                <q-input dense dark filled v-model="pressureRL" type="number" label="Hinten Links"
+                         @keyup="setSinglePressure(pressureRL,'RL')"/>
+                <q-input dense dark filled v-model="pressureRR" type="number" label="Hinten Rechts"
+                         @keyup="setSinglePressure(pressureRR,'RR')"/>
+              </div>
+            </q-card-section>
+            <q-separator dark></q-separator>
+            <q-card-section class="q-gutter-y-sm">
+              <div class="row text-white text-subtitle1 justify-center">Berechneter Reifendruck</div>
+              <div class="row q-gutter-x-sm">
+                <q-input disable dense dark filled v-model="pressureCalcFL" label="Vorne Links"/>
+                <q-input disable dense dark filled v-model="pressureCalcFR" label="Vorne Rechts"/>
+              </div>
+              <div class="row q-gutter-x-sm">
+                <q-input disable dense dark filled v-model="pressureCalcRL" label="Hinten Links"/>
+                <q-input disable dense dark filled v-model="pressureCalcRR" label="Hinten Rechts"/>
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
       </q-tab-panel>
       <q-tab-panel name="used" class="row justify-center full-height q-gutter-lg">
         <div class="column">
-          <q-table
-              title="Benutzt"
-              :rows="tireSetRows_used"
-              :columns="tireSetColumns_used"
-              row-key="id"
-              class="bg-primary"
-              dark
-              no-data-label="Table is empty"
-              :loading="loading_used"
-          >
+          <q-table title="Benutzt"
+                   :rows="tireSetRows_used"
+                   :columns="tireSetColumns_used"
+                   row-key="id"
+                   class="bg-primary"
+                   dark
+                   no-data-label="Table is empty"
+                   :loading="loading_used">
             <template v-slot:loading>
               <q-inner-loading showing color="primary"/>
             </template>
@@ -132,16 +158,14 @@
               </q-tr>
               <q-tr v-show="props.expand" :props="props">
                 <q-td colspan="100%">
-                  <q-table
-                      title="Tires"
-                      :rows="props.row.tires"
-                      :columns="tireColumns"
-                      row-key="tireID"
-                      hide-bottom
-                      dark
-                      class="bg-primary"
-                      bordered
-                  >
+                  <q-table title="Tires"
+                           :rows="props.row.tires"
+                           :columns="tireColumns"
+                           row-key="tireID"
+                           hide-bottom
+                           dark
+                           class="bg-primary"
+                           bordered>
                   </q-table>
                 </q-td>
               </q-tr>
@@ -229,7 +253,17 @@ export default {
       tireSetColumns_used,
       tireColumns,
       tireSetRows_inStock: [],
-      tireSetRows_used: []
+      tireSetRows_used: [],
+      tireTempCold: null,
+      pressureRL: null,
+      pressureFL: null,
+      pressureRR: null,
+      pressureFR: null,
+      pressureCalcRL: null,
+      pressureCalcFL: null,
+      pressureCalcRR: null,
+      pressureCalcFR: null,
+      pressureVars: [],
     }
   },
   methods: {
@@ -321,8 +355,7 @@ export default {
                 }
               }
           )
-    }
-    ,
+    },
     tireSetStartHeatingTimer(tireSet) {
       const apiUrl = this.$store.state.host.api_url
       const url = `${apiUrl}/tireset/update/${tireSet.id}/heatingStart`
@@ -347,8 +380,7 @@ export default {
                 }
               }
           )
-    }
-    ,
+    },
     tireSetEndHeatingTimer(tireSet) {
       const apiUrl = this.$store.state.host.api_url
       const url = `${apiUrl}/tireset/update/${tireSet.id}/heatingStop`
@@ -373,8 +405,7 @@ export default {
                 }
               }
           )
-    }
-    ,
+    },
     tireSetStatusUsed(tireSet) {
       const apiUrl = this.$store.state.host.api_url
       const url = new URL(`${apiUrl}/tireset/update/${tireSet.id}/status`)
@@ -406,11 +437,79 @@ export default {
                 }
               }
           )
+    },
+    getPressureVars() {
+      const apiUrl = this.$store.state.host.api_url
+      let url = apiUrl + '/race/pressureVars'
+      const jwt = this.$store.state.user.jwt
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + jwt
+        },
+      }
+      let resp1
+      fetch(url, requestOptions)
+          .then(response => {
+            resp1 = response
+            return response.json()
+          })
+          .then(data => {
+            this.pressureVars = data
+          })
+    },
+    setAllPressure() {
+      if (this.pressureFL !== null) {
+        console.log('fl')
+        this.pressureCalcFL = Number(this.pressureFL) * (Number(this.tireTempCold) + this.pressureVars[0]) /
+            this.pressureVars[1] + this.pressureVars[2] * (Number(this.tireTempCold) -
+                this.pressureVars[3]) / this.pressureVars[1]
+      }
+      if (this.pressureFR !== null) {
+        this.pressureCalcFR = Number(this.pressureFR) * (Number(this.tireTempCold) + this.pressureVars[0]) /
+            this.pressureVars[1] + this.pressureVars[2] * (Number(this.tireTempCold) -
+                this.pressureVars[3]) / this.pressureVars[1]
+      }
+      if (this.pressureRL !== null) {
+        this.pressureCalcRL = Number(this.pressureRL) * (Number(this.tireTempCold) + this.pressureVars[0]) /
+            this.pressureVars[1] + this.pressureVars[2] * (Number(this.tireTempCold) -
+                this.pressureVars[3]) / this.pressureVars[1]
+      }
+      if (this.pressureRR !== null) {
+        this.pressureCalcRR = Number(this.pressureRR) * (Number(this.tireTempCold) + this.pressureVars[0]) /
+            this.pressureVars[1] + this.pressureVars[2] * (Number(this.tireTempCold) -
+                this.pressureVars[3]) / this.pressureVars[1]
+      }
+    },
+    setSinglePressure(pressure, tire) {
+      if (this.tireTempCold === null) return
+      const calcValue = Number(pressure) * (Number(this.tireTempCold) + this.pressureVars[0]) /
+          this.pressureVars[1] + this.pressureVars[2] * (Number(this.tireTempCold) -
+              this.pressureVars[3]) / this.pressureVars[1]
+      switch (tire) {
+        case 'FL': {
+          this.pressureCalcFL = calcValue
+          break
+        }
+        case 'FR': {
+          this.pressureCalcFR = calcValue
+          break
+        }
+        case 'RL': {
+          this.pressureCalcRL = calcValue
+          break
+        }
+        case 'RR': {
+          this.pressureCalcRR = calcValue
+          break
+        }
+      }
     }
-    ,
-  },
+  }
+  ,
   mounted() {
     this.getAllTireSets()
+    this.getPressureVars()
   }
 }
 </script>
